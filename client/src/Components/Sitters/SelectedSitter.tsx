3d5819891dec17c0850c816fec066d240ea4ce7d
Prabodhi <prabodhi38@gmail.com>
2023-03-28 11:28:12 +0200

    display selected nanny infor

--- client/src/Components/Sitters/SelectedSitter.tsx
+++ client/src/Components/Sitters/SelectedSitter.tsx
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import SitterType from '../../../../types';
 import api from '../../Api/api';
 
-// type Props = {
-//   sitter: SitterType;
-// }
-
 export default function SelectedSitter() {
+  const [nannyinfo, setNannyinfo] = useState<SitterType>();
   const searchParams = new URLSearchParams(useLocation().search);
   const id = searchParams.get('id');
-  console.log(id);
-  const getSitterById = async (id: any) => {
-    try {
-      const response = await api.get(`/api/sitters/${id}`);
-      console.log(response);
-      // setSitters(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+
+  useEffect(() => {
+    const getSitterById = async (id: any) => {
+      try {
+        const response = await api.get(`/api/sitters/${id}`);
+        console.log(response.data);
+        setNannyinfo(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getSitterById(id);
+  }, []);
+
   return (
     <div>
-      <h2>sitter 01</h2>
-      {/* <p>{props.sitter.description}</p> */}
-      {/* Render additional details here */}
+      {nannyinfo && (
+        <>
+          <h2>Sitter info:</h2>
+          <p>Name: {nannyinfo.name}</p>
+        </>
+      )}
     </div>
   );
 }
