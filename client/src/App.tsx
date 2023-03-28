c12787ce7770698431cc62084db1ca50ac0e9c65
Prabodhi <prabodhi38@gmail.com>
2023-03-28 12:40:01 +0200

    adding css to home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LogIn from './Components/LogIn/LogIn';
 import Register from './Components/Register/Register';
 import Calendar from './Components/Calendar/Calendar';
+import Header from './Components/Header/Header';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <div className="App">
-      Hello Tiny Sitters
-    </div>
-    <LogIn/>
-    <Register/>
-    <Calendar/>
+    <Header/>
+    <h1 className='main__title'>You go out... <br /> we take care <br /> of your <br /> little ones </h1>
+    <div className='main__btns'>
+      <Link className='main__link' to='/login'>LogIn</Link>
+      <Link className='main__link' to='/register'>Create Account</Link>
+      <Link className='main__link pick__date' to='/calendar'>Pick Date</Link>
+      </div>
     </>
   );
 }
---
+++ client/src/Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from './tinysitters_logo.svg';
+import "./header.css"
+const Header = () => {
+
+  return (
+    <img className="salt__header--logo" src={logo} alt="logo"/>
+  )
+}
+
+export default Header
